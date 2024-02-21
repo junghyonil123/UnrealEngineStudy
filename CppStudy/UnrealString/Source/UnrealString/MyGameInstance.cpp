ba4b1dbbf5c0187e5f4a30c0274f087edabf4559
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2024-02-21 13:51:05 +0900

    UnrealString

---
+++ CppStudy/UnrealString/Source/UnrealString/MyGameInstance.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	TCHAR logCharArray[] = TEXT("Hello Unreal");
+	UE_LOG(LogTemp, Log, logCharArray);
+
+	FString LogCharString = logCharArray;
+	UE_LOG(LogTemp, Log,TEXT("%s"), *LogCharString);
+	
+	const TCHAR* LongCharPtr = *LogCharString;
+	
+	//UE_LOG(LogTemp, Log,TEXT("%s"), LongCharPtr);
+	//UE_LOG(LogTemp, Log,TEXT("%s"), *LongCharPtr);
+
+	FString left, Right;
+	if (LogCharString.Split(TEXT(" "), &left, &Right))
+	{
+		UE_LOG(LogTemp, Log,TEXT("Split Test: %s 와 %s"), *left, *Right);
+	}
+
+}
---
+++ CppStudy/UnrealString/Source/UnrealString/UnrealStringGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealStringGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSTRING_API AUnrealStringGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
