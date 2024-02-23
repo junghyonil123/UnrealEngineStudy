62ebfba5362994bb8bc4298668993f26648aa44e
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2024-02-23 17:38:25 +0900

    Container

---
+++ CppStudy/Container/Source/Container/Container.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Container.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Container, "Container" );
---
+++ CppStudy/Container/Source/Container/ContainerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ContainerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONTAINER_API AContainerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
