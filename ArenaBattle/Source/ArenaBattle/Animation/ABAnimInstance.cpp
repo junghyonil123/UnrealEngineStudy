6547e03189a520d6b9406b48d72008c60ea62315
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2024-02-27 11:16:17 +0900

    Player Animation

---
+++ ArenaBattle/Source/ArenaBattle/Animation/ABAnimInstance.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/ABAnimInstance.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UABAnimInstance::UABAnimInstance()
+{
+	MovingThresould = 3.0f;
+	JumpingThresould = 100.0f;
+}
+
+void UABAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	Owner = Cast<ACharacter>(GetOwningActor());
+	if (Owner)
+	{
+		Movement = Owner->GetCharacterMovement();
+	}
+}
+
+void UABAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (Movement)
+	{
+		Velocity = Movement->Velocity;
+		GroundSpeed = Velocity.Size2D();
+		bIsIdle = GroundSpeed < MovingThresould;
+		bIsFalling = Movement->IsFalling();
+		bIsJumping = bIsFalling & (Velocity.Z > JumpingThresould);
+	}
+}
