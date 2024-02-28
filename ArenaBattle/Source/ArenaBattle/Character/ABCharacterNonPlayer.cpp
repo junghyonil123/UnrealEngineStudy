3f18607826eca59efef2f96a33bedd8d22cb4204
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2024-02-28 11:46:32 +0900

    Character Attack

---
+++ ArenaBattle/Source/ArenaBattle/Character/ABCharacterNonPlayer.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/ABCharacterNonPlayer.h"
+
+AABCharacterNonPlayer::AABCharacterNonPlayer()
+{
+}
+
+void AABCharacterNonPlayer::SetDaed()
+{
+	Super::SetDaed();
+
+	FTimerHandle DeadTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(DeadTimerHandle, FTimerDelegate::CreateLambda(
+		[&]()
+		{
+			Destroy();
+		}
+	), DeadEventDelayTime, false);
+}
